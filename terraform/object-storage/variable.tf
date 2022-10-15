95e1f013934e066e987558bdf6ffcd15cf183cf9
Alekseev Anton <alekseev.apa@edu.spbstu.ru>
2022-10-15 17:16:19 +0300

    object storage for tfstate

---
+++ terraform/object-storage/variable.tf
@@ -0,0 +1,35 @@
+variable "yc_region" {
+  description = "регион"
+  type        = string
+  default     = "ru-central1-a"
+}
+
+variable "yc_folder_id" {
+  description = "идентификатор каталога"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_cloud_id" {
+  description = "идентификатор облака"
+  type        = string
+  sensitive   = true
+}
+
+variable "yc_token" {
+  description = "IAM- или OAuth-токен"
+  type        = string
+  sensitive   = true
+}
+
+variable "sa_name" {
+  description = "имя сервисного аккаунта"
+  type        = string
+  default     = "s3-storage-service"
+}
+
+variable "bucket_name" {
+  description = "имя бакета"
+  type        = string
+  default     = "s3-terraform-state"
+}
